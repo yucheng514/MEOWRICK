b4c6ecbca5ff80744d0bb5df87b6c0c723263aa0
daluozha <561890199@qq.com>
2022-05-04 20:23:56 +0800

    init & finish day01

---
+++ src/App.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+import expandingCards from './components/day01-expanding-cards.vue'
+</script>
+
+<template>
+<expandingCards></expandingCards>
+</template>
+
+<style>
+
+</style>
---
+++ src/components/day01-expanding-cards.vue
@@ -0,0 +1,128 @@
+<script lang="ts">
+export default {
+  data() {
+    return {
+
+      panelsData: [
+        {
+          id: 1,
+          url: 'https://images.unsplash.com/photo-1558979158-65a1eaa08691?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80',
+          text: 'Explore The World'
+        },
+        {
+          id: 2,
+          url: 'https://images.unsplash.com/photo-1572276596237-5db2c3e16c5d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80',
+          text: 'Wild Forest'
+        },
+        {
+          id: 3,
+          url: 'https://images.unsplash.com/photo-1507525428034-b723cf961d3e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1353&q=80',
+          text: 'Sunny Beach'
+        },
+        {
+          id: 4,
+          url: 'https://images.unsplash.com/photo-1551009175-8a68da93d5f9?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1351&q=80',
+          text: 'City on Winter'
+        },
+        {
+          id: 5,
+          url: 'https://images.unsplash.com/photo-1549880338-65ddcdfd017b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80',
+          text: 'Mountains - Clouds'
+        }
+      ]
+    }
+  },
+  mounted() {
+    const panels = document.querySelectorAll('.panel')
+
+    panels.forEach(panel => {
+      panel.addEventListener('click', () => {
+        removeActiveClasses()
+        panel.classList.add('active')
+      })
+    })
+
+    function removeActiveClasses() {
+      panels.forEach(panel => {
+        panel.classList.remove('active')
+      })
+    }
+  }
+}
+
+</script>
+
+<template>
+  <div class="container">
+    <div v-for="item in panelsData" :id="item.id" class="panel" :class="item.id == 1 ? 'active' : ''"
+      :style="{ 'background-image': `url(${item.url})` }">
+      <h3>{{ item.text }}</h3>
+    </div>
+  </div>
+</template>
+
+<style>
+@import url('https://fonts.googleapis.com/css?family=Muli&display=swap');
+
+* {
+  box-sizing: border-box;
+}
+
+body {
+  font-family: 'Muli', sans-serif;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  height: 100vh;
+  overflow: hidden;
+  margin: 0;
+}
+
+.container {
+  display: flex;
+  width: 90vw;
+}
+
+.panel {
+  background-size: cover;
+  background-position: center;
+  background-repeat: no-repeat;
+  height: 80vh;
+  border-radius: 50px;
+  color: #fff;
+  cursor: pointer;
+  flex: 0.5;
+  margin: 10px;
+  position: relative;
+  -webkit-transition: all 700ms ease-in;
+}
+
+.panel h3 {
+  font-size: 24px;
+  position: absolute;
+  bottom: 20px;
+  left: 20px;
+  margin: 0;
+  opacity: 0;
+}
+
+.panel.active {
+  flex: 5;
+}
+
+.panel.active h3 {
+  opacity: 1;
+  transition: opacity 0.3s ease-in 0.4s;
+}
+
+@media (max-width: 480px) {
+  .container {
+    width: 100vw;
+  }
+
+  .panel:nth-of-type(4),
+  .panel:nth-of-type(5) {
+    display: none;
+  }
+}
+</style>
